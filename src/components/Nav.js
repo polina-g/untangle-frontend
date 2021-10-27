2549eaadef1d5c0b2bec72df38385f621e30ea44
polina-g <ignatieva.polina@gmail.com>
2021-10-27 12:44:35 -0400

    wip: adds nav style

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,30 +1,80 @@
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
 
 const Nav = (props) => {
+  const StyledHeader = styled.nav`
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding-left: 1rem;
+    height: 5rem;
+    background-color: purple;
+    color: white;
+    box-shadow: 1px 1px 3px 2px #808080;
+    nav {
+      display: flex;
+      align-items: center;
+      ul {
+        display: flex;
+        list-style: none;
+        align-items: center;
+        li {
+          margin-right: 1.5rem;
+          font-size: 1.5rem;
+          font-weight: 500;
+          a {
+            text-decoration: none;
+            color: inherit;
+          }
+          &::last-child:hover {
+            cursor: pointer;
+          }
+        }
+      }
+    }
+  `;
   return (
-    <nav className="nav">
-        <Link to="/">
-            <div>Home</div>
-        </Link>
-        <Link to="/login">
-            <div>Log In</div>
-        </Link>
-        <Link to="/register">
-            <div>Sign Up</div>
-        </Link>
-        <Link to="/dashboard">
-            <div>Dashboard</div>
-        </Link>
-        <Link to="/entries/new">
-            <div>New Entry</div>
-        </Link>
-        <Link to="/entries">
-            <div>All Entries</div>
-        </Link>
-        <Link to="/">
-            <div>Log Out</div>
-        </Link>
-    </nav>
+    <StyledHeader className="nav">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">
+                <div>Home</div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/login">
+                <div>Log In</div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/register">
+                <div>Sign Up</div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/dashboard">
+                <div>Dashboard</div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/entries/new">
+                <div>New Entry</div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/entries">
+                <div>All Entries</div>
+            </Link>
+          </li>
+          <li>
+            <Link to="/">
+                <div>Log Out</div>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </StyledHeader>
 );
   };
 
