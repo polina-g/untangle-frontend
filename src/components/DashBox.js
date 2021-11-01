78b0369e22ec26d14c00b1cc2a17250c7a21aa79
polina-g <ignatieva.polina@gmail.com>
2021-11-01 15:10:42 -0400

    wip: material ui adds box link styling

--- src/components/DashBox.js
+++ src/components/DashBox.js
@@ -1,22 +1,37 @@
 import { StyledDashBox, LinkStyle } from "../styles";
 import { Link } from "react-router-dom";
 
+//Material UI imports
+import Box from '@mui/material/Box'
+import Paper from "@mui/material/Paper";
+
 const DashBox = ({title, link, createEntry, token, data}) => {
     return (
-    <LinkStyle>
-        <Link to={{
-            pathname: `${link}`,
-            createEntry: createEntry,
-            state: {
-                token: token,
-                data: data
-            }
-        }}>
-            <StyledDashBox>
-                <h2>{title}</h2>
-            </StyledDashBox>
-        </Link>
-    </LinkStyle>
+    <Paper elevation={10} sx={{ width: 250, height: 250}}>
+        <LinkStyle>
+            <Link to={{
+                pathname: `${link}`,
+                createEntry: createEntry,
+                state: {
+                    token: token,
+                    data: data
+                }
+            }}>
+                <Box
+                    sx={{
+                        width: 250,
+                        height: 250,
+                        backgroundColor: '#eeb1b1',
+                        '&:hover': {
+                            backgroundColor: '#eeb1b1',
+                            opacity: [0.9, 0.8, 0.7],
+                        }
+                    }}>
+                    {title}
+                </Box>
+            </Link>
+        </LinkStyle>
+    </Paper>
     );
   };
 
