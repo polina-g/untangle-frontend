740467e1153c467cda31406c397e0cfc237373f6
polina-g <ignatieva.polina@gmail.com>
2021-11-02 14:08:03 -0400

    wip: adds form styling with material ui

--- src/components/EntryForm.js
+++ src/components/EntryForm.js
@@ -1,94 +1,126 @@
 import { StyledForm } from "../styles";
 
+import Box from '@mui/material/Box'
+import Button from '@mui/material/Button'
+import Paper from '@mui/material/Paper'
+import TextField from '@mui/material/TextField'
+import RadioGroup from "@mui/material/RadioGroup";
+import Radio from "@mui/material/Radio";
+import Slider from "@mui/material/Slider";
+import FormLabel from "@mui/material/FormLabel";
+import FormControl from "@mui/material/FormControl";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { FormGroup } from "@mui/material";
+
+
+
 const EntryForm = ({handleChange, handleSubmit, formState, isEdit}) => {
-  console.log(handleChange)
     return (
-    <StyledForm onSubmit={handleSubmit}>
-      <p>How are you feeling?</p>
-        <div>
-          <div className="column">
-            <label htmlFor="feeling"><img src='https://i.imgur.com/kj80WQP.png'/></label>
-            <input 
-              value="1"
-              onChange={handleChange}
-              name="feeling"
-              type="radio"      
-            />
-          </div>
-          <div className="column">
-            <label htmlFor="feeling"><img src='https://i.imgur.com/dPeJdMK.png' /></label>
-            <input 
-              value="2"
-              onChange={handleChange}
-              name="feeling"
-              type="radio"
-            />
-          </div>
-          <div className="column">
-            <label htmlFor="feeling"><img src='https://i.imgur.com/X3Oeeja.png'/></label>
-            <input 
-              value="3"
-              onChange={handleChange}
-              name="feeling"
-              type="radio"
-            />
-          </div>
-          <div className="column">
-            <label htmlFor="feeling"><img src='https://i.imgur.com/Wkm4rbM.png'/></label>
-            <input 
-              value="4"
-              onChange={handleChange}
-              name="feeling"
-              type="radio"
-            />
-          </div>
-          <div className="column">
-            <label htmlFor="feeling"><img src='https://i.imgur.com/9DlOwAL.png'/></label>
-            <input 
-              value="5"
-              onChange={handleChange}
-              name="feeling"
-              type="radio"
-            />
-          </div>
-        </div>
-      <input
-        onChange={handleChange}
-        value={formState.emotion}
-        name="emotion"
-        type="text"
-        placeholder="Emotion"
-      />
-      <label htmlFor="intensity">Intensity</label>
-      <input
-        onChange={handleChange}
-        value={formState.intensity}
-        name="intensity"
-        type="range"
-        min="0"
-        max="100"
-      />
-      <input
-        onChange={handleChange}
-        value={formState.thought}
-        name="thought"
-        type="text"
-        placeholder="Thought"
-      />
-      <label HTMLfor="rob">Rate of Belief</label>
-      <input
-        onChange={handleChange}
-        value={formState.rob}
-        name="rob"
-        type="range"
-        min="0"
-        max="100"
-      />
-      <textarea
+      <Box 
+      sx={{width: '70%', mt:5, ml:'15%'}}
+      justifyContent="center"
+      alignItems="center">
+
+      <Paper 
+        elevation={5} 
+        sx={{width: '100%', p: '1rem', flexDirection: 'column',
+        alignItems: 'center'}}>
+        
+    <form onSubmit={handleSubmit}>
+      <RadioGroup row>
+        <FormControlLabel 
+          value="1" 
+          control={<Radio />}
+          onChange={handleChange}
+          label={<img src='https://i.imgur.com/kj80WQP.png'/>}
+          labelPlacement='top'
+          name="feeling"
+        />
+        <FormControlLabel 
+          value="2" 
+          control={<Radio />}
+          onChange={handleChange}
+          label={<img src='https://i.imgur.com/dPeJdMK.png' />}
+          labelPlacement='top'
+          name="feeling"
+        />
+        <FormControlLabel 
+          value="3" 
+          control={<Radio />}
+          onChange={handleChange}
+          label={<img src='https://i.imgur.com/X3Oeeja.png'/>}
+          labelPlacement='top'
+          name="feeling"
+        />
+        <FormControlLabel 
+          value="4" 
+          control={<Radio />}
+          onChange={handleChange}
+          label={<img src='https://i.imgur.com/Wkm4rbM.png'/>}
+          labelPlacement='top'
+          name="feeling"
+        />
+        <FormControlLabel 
+          value="5" 
+          control={<Radio />}
+          onChange={handleChange}
+          label={<img src='https://i.imgur.com/9DlOwAL.png'/>}
+          labelPlacement='top'
+          name="feeling"
+        />
+      </RadioGroup>
+       
+      <FormGroup row>
+        <TextField
+          onChange={handleChange}
+          value={formState.emotion}
+          name="emotion"
+          type="text"
+          placeholder="Emotions"
+          label="List your emotions"
+          sx={{width:'50%', mr:5}}
+        />
+        <Slider 
+          valueLabelDisplay="auto" 
+          min={0}
+          max={100}
+          onChange={handleChange}
+          value={formState.intensity}
+          name="intensity"
+          sx={{width:'30%'}}
+        />
+      </FormGroup>
+
+      <FormGroup row> 
+        <TextField
+          onChange={handleChange}
+          value={formState.thought}
+          name="thought"
+          type="text"
+          placeholder="Thought"
+          label="Most distressing thought"
+          sx={{width:'50%', mr:5 }}
+          
+        />
+        <Slider
+          valueLabelDisplay="auto" 
+          min={0}
+          max={100}
+          onChange={handleChange}
+          value={formState.rob}
+          name="rob"
+          sx={{width:'30%'}}
+        />
+      </FormGroup>
+      <TextField
         onChange={handleChange}
         value={formState.situation}
         name="situation"
-        placeholder="situation"
+        placeholder="Briefly Describe The Situation"
+        label="Situation"
+        fullWidth
+        multiline
+        rows={6}
       />
       <div className="markPrivate">
         <label htmlFor="private">Mark Private</label>
@@ -100,7 +132,11 @@ const EntryForm = ({handleChange, handleSubmit, formState, isEdit}) => {
         />
       </div>
       <input type="submit" value={isEdit ? "Submit" : "Add Entry" }/>
-    </StyledForm>
+    </form>
+
+    </Paper>
+
+    </Box>
     );
   };
 
