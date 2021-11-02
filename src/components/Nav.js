6e6c8cc36345a900c541c0a932d5d537cf79181e
polina-g <ignatieva.polina@gmail.com>
2021-11-01 22:21:32 -0400

    wip: adds navbar styling

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,6 +5,9 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import AccountCircle from '@mui/icons-material/AccountCircle';
@@ -15,6 +18,7 @@ import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 
 import { StyledLogo } from '../styles';
+import { LinkStyle } from '../styles';
 
 const Nav = ({user}) => {
 
@@ -24,61 +28,105 @@ const Nav = ({user}) => {
     <AppBar position='static'>
       <Toolbar>
         <Link to ='/'>
-            <Typography variant="h6">
-              <StyledLogo  src="https://i.imgur.com/MPEA9T2.png"/>
-            </Typography>
-          </Link>
-        <Typography
-            variant="h3"
-            noWrap
-            component="div"
-            color='secondary'
-            sx={{ ml:3 }}
-          >
-            Untangle
-        </Typography>
+          <Typography variant="h6">
+            <StyledLogo  src="https://i.imgur.com/MPEA9T2.png"/>
+          </Typography>
+        </Link>
            { user ? 
              <>
-               <li>
-                 <Link to="/dashboard">
-                     Dashboard
-                 </Link>
-               </li>
-               <li>
-                 <Link to={{
-                   pathname: '/entries/new',
-                 }}>
-                 New Entry
-                 </Link>
-               </li>
-               <li>
-               <Link to={{
-                   pathname: '/entries',
-                 }}>
-                 All Entries
-                 </Link>
-               </li>
-               <li onClick={logOut}>
-                   Log Out
-               </li>
+              <LinkStyle>
+                <Link to="/dashboard">
+                  <Button
+                    variant="contained"
+                    color='secondary'
+                    size="large"
+                    sx={{ml:3}}
+                  >
+                      Dashboard
+                  </Button>
+                </Link>
+              </LinkStyle>
+                <LinkStyle>
+                  <Link to='/entries/new'>
+                    <Button 
+                      variant='contained' 
+                      color='secondary'
+                      size="large"
+                      sx={{ml:3}}
+                    >
+                      New Entry
+                    </Button>
+                  </Link>
+                </LinkStyle>
+                <LinkStyle>
+                  <Link to='/entries'>
+                    <Button 
+                      variant='contained' 
+                      color='secondary'
+                      size="large"
+                      sx={{ml:3}}
+                    >
+                      All Entries
+                    </Button>
+                  </Link>
+                </LinkStyle>
+                <Typography
+                    variant="h3"
+                    noWrap
+                    component="div"
+                    color='secondary'
+                    sx={{ flexGrow:1 }}
+                >
+                    Untangle
+                </Typography>
+                <Button 
+                  variant='contained' 
+                  color='secondary'
+                  sx={{ml:3}}
+                  size="large"
+                  onClick={logOut}
+                >
+                  <LogoutRoundedIcon sx={{mr:1}}/>
+                  Log Out
+                </Button>
              </>
             :
              <> 
-               <li>
-                 <Link to="/">
-                     Home
-                 </Link>
-               </li>
-               <li>
-                 <Link to="/login">
-                     Log In
-                 </Link>
-               </li>
-               <li>
-                 <Link to="/register">
-                     Sign Up
-                 </Link>
-               </li>
+              <Typography
+                  variant="h3"
+                  noWrap
+                  component="div"
+                  color='secondary'
+                  sx={{ flexGrow:1 }}
+              >
+                  Untangle
+              </Typography>
+              <LinkStyle>
+                <Link to="/login">
+                  <Button 
+                    variant='contained' 
+                    color='secondary'
+                    sx={{ml:3}}
+                    size="large"
+                    onClick={logOut}
+                  >
+                    Log In
+                  </Button>
+                </Link>
+              </LinkStyle>
+              <LinkStyle>
+                <Link to="/register">
+                  <Button 
+                    variant='contained' 
+                    color='secondary'
+                    sx={{ml:3}}
+                    size="large"
+                    onClick={logOut}
+                  >
+                    Sign Up
+                  </Button>
+                </Link>
+              </LinkStyle>
              </>
          }
       </Toolbar>
